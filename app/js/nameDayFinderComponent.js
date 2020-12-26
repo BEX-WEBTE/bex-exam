452a9373ff0c2da9a672360f82a3f5948af6a527
matejPeluha <matej.peluha@gmail.com>
2020-12-26 14:58:44 +0100

    UPDATE: pripravenie frontendu, zakladne preklikavanie, podrobnejsi navrh komponentu

--- app/js/nameDayFinderComponent.js
+++ app/js/nameDayFinderComponent.js
@@ -1,6 +1,77 @@
 class NameDayFinderComponent extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = '<span class="thumbnail-container-event-title ">Dnes meniny oslavuje </span>';
+        this.innerHTML = '<div id="text-div">'
+                            + '<span id="intro-text-name" class="thumbnail-container-event-title ">Dnes meniny oslavuje:'
+                            + '<br><em id="text-name" class="bold-max"> Matej </em>'
+                            + '<span class="button-with-icon">'
+                            + '<button id="more-names-button">'
+                            + '<a id="more-names-a" data-toggle="collapse" href="#more-names" aria-expanded="false" aria-controls="more-names" >rozšírené</a>'
+                            + '<i class="far fa-caret-square-down"></i>'
+                            + '</button>'
+                            + '</span>'
+                            + '</span>'
+                            + '<p id="more-names" class="collapse">Žiadne ďalšie mená</p>'
+                            + '<br><span id="intro-text-holiday" class="thumbnail-container-event-title ">Dnes oslavujeme sviatok:'
+                            + '<br><em id="text-holiday" class="bold-max"> Vianoce </em></span></div>';
+
+
+        this.innerHTML += '<div class="nameday-input-div"><label for="date-input">Dňa  </label>'
+                            + '<input class="nameday-input" type="date" id="date-input" name="date-input"></div>';
+
+        this.innerHTML += '<div class="nameday-input-div"><label for="name-input">Meno  </label>'
+                            + '<input class="nameday-input" type="text" id="name-input" name="name-input">';
+
+        this.innerHTML += '<div class="nameday-input-div"><label for="holiday-input">Sviatok  </label>'
+                            + '<input class="nameday-input" type="text" id="holiday-input" name="holiday-input"></div>';
+
+        this.innerHTML += '<ul id="country-chooser">'
+                            + '<li class="active-country country"><span>SK</span></li>'
+                            + '<li class="country"><span>CZ</span></li>'
+                            + '<li class="country"><span>HU</span></li>'
+                            + '<li class="country"><span>PL</span></li>'
+                            + '<li class="country"><span>AT</span></li>'
+                            + '</ul>'
+
+
+
+        function initDateInput(){
+            const dateInput = document.getElementById("date-input");
+            dateInput.valueAsDate = new Date();
+        }
+
+        function initNameInput(){
+            const nameInput = document.getElementById("name-input");
+            nameInput.value = "Matej";
+        }
+
+        function getName(date){
+
+        }
+
+        function initHolidayInput(){
+            const holidayInput = document.getElementById("holiday-input");
+            holidayInput.value = "Vianoce";
+        }
+
+        function initCountryChooser(){
+            const allCountries = document.getElementsByClassName("country");
+
+            for(let country of allCountries)
+                country.addEventListener("click", function (){changeNameDayCountry(country)});
+
+        }
+
+        function changeNameDayCountry(country){
+            const activeCountry = document.getElementsByClassName("active-country");
+            activeCountry[0].classList.remove("active-country");
+
+            country.classList.add("active-country");
+        }
+
+        initDateInput();
+        initNameInput();
+        initHolidayInput();
+        initCountryChooser();
     }
 }
 
