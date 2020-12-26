d3f40b6a581a7733c4f24e25949be96f7e82695f
matejPeluha <matej.peluha@gmail.com>
2020-12-26 10:49:49 +0100

    ADD: vytvorenie komponentu podla predlohy

--- app/js/nameDayFinderComponent.js
+++ app/js/nameDayFinderComponent.js
@@ -1,6 +1,10 @@
-class NameDayFinderComponent{
+class NameDayFinderComponent extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = '<span class="thumbnail-container-event-title ">Dnes meniny oslavuje </span>';
+    }
+}
 
 
-
-
+if(!customElements.get('nameday-finder')) {
+    customElements.define('nameday-finder', NameDayFinderComponent);
 }
