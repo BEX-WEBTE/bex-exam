4f6800eb9d5522dc1b7b4915a790a4d22261b5ad
LubosSremanak <luboss2106@gmail.com>
2020-12-29 14:18:15 +0100

    Pridaná funkcia na rekurzívne načítanie menu z jsona.

--- app/three-level-menu/js/three-level-menu.component.js
+++ app/three-level-menu/js/three-level-menu.component.js
@@ -1,9 +1,19 @@
 class ThreeLevelMenuComponent extends HTMLElement {
 
+
+    menuContent;
+
     constructor() {
         super();
+        this.menuContent = {
+            "id": "",
+            "title": "",
+            "path": "",
+            "child": [],
+        }
         this.dom = this.attachShadow({mode: 'open'});
         this.loadHtml();
+        this.loadMenuContent();
 
     }
 
@@ -15,10 +25,77 @@ class ThreeLevelMenuComponent extends HTMLElement {
             });
     }
 
-
-
     getElementById(id) {
         return this.dom.querySelector('#' + id);
 
     }
+
+    loadMenuContent() {
+        fetch("app/three-level-menu/menu-content.json")
+            .then(response => response.json())
+            .then(json => {
+                this.menuContent = json.menuContent;
+                this.crateListFromMenuContent();
+            });
+    }
+
+    createHtmlList() {
+        let list = document.createElement("ul");
+        list.classList.add("navigation-bar-inner-row");
+        return list;
+    }
+
+    createHtmlListItem() {
+        let listItem = document.createElement("li");
+        listItem.classList.add("item");
+        return listItem;
+    }
+
+    createHtmlLink() {
+        return document.createElement("a");
+    }
+
+
+    createHtmlDiv() {
+        let div = document.createElement("div");
+        div.classList.add("item-link");
+        return div;
+    }
+
+    setAttributeAndTextOfLinkFromItem(link, item) {
+        link.setAttribute("href", item.path)
+        link.innerText = item.title;
+    }
+
+    crateListFromMenuContent() {
+        let list = this.getElementById("menu");
+        this.menuContent.forEach((item) => {
+            this.createList(list, item);
+        });
+    }
+
+    searchInChildrenAndCreateListForGrandChildren(item, listItem) {
+        let newList = this.createHtmlList();
+        item.child.forEach((child) => {
+            listItem.appendChild(newList);
+            this.createList(newList, child);
+        })
+    }
+
+    createList(list, item) {
+        let listItem = this.createHtmlListItem();
+        let link = this.createHtmlLink();
+        let div = this.createHtmlDiv();
+        this.setAttributeAndTextOfLinkFromItem(link, item);
+        div.appendChild(link)
+        listItem.appendChild(div);
+        list.appendChild(listItem);
+
+        if (item.child) {
+            this.searchInChildrenAndCreateListForGrandChildren(item, listItem);
+        }
+
+    }
+
+
 }
